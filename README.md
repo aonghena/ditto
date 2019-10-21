84145687aeb57f138103f57e4cd858ee1dde5a2a
Alex O <8991237+aonghena@users.noreply.github.com>
2019-10-21 17:31:29 -0400

    File Input
    Allows File Input instead of link

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ditto
 4155 project
+
+
+
+### info  
+test.js will need to be altered to run on local machines since it depends on a server to host user submitted photos
+  
+#### todo:  
+-categories  
+-3 photo output  
+-stat output  
+-mobileview  
+-social media sharing  
+-other  
--- test.js
+++ test.js
@@ -1,39 +1,70 @@
 var request = require('request-promise');
-var faceId = "";
 var result = "";
 var express= require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var formidable = require('formidable');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const fs = require('fs');
-
 let rawdata = fs.readFileSync('billionaires.json');
 let microsoftList = JSON.parse(rawdata);
+const path = require("path");
+http = require('http'),
+util = require('util');
 
-
-app.listen(3000);
+app.listen(80);
 
 app.set('view engine', 'ejs');
 
 app.use('/asset', express.static("asset"));
 
 app.get('/', function(req, res){
-    console.log(req.query);
     res.render('index', {qs: req.query});
 });
 
+//Orient Bambino
+//to get userinput image for output and azure upload
+app.get("/:image", (req, res) => {
+    console.log("test")
+    res.sendFile(path.join(__dirname, "./uploads/" +req.params.image));
+});
+
+//captures user image from forum
+async function getPhoto(req){
+    return new Promise(function(resolve, reject){formidable.IncomingForm().parse(req)
+        .on('fileBegin', async function(name, file){
+            console.log(typeof file.name);
+            if(file.name != ""){
+                //change if runnning locally
+                file.path = __dirname + "/uploads/" + file.name.toUpperCase()
+                face = "http://157.245.127.122/" + file.name.toUpperCase() + " "
+                ///
+                face.trim()
+                resolve(face);
+            }else{
+                resolve("fuck");
+            }
+        })
+    })
+}
 //just outputs top face for now
 app.post('/find', urlencodedParser,async function(req, res){
-    var face = req.body.faceInput;
-    var faceId = await detect(face, 'billion');
-    if(faceId == undefined){
-        res.render('result', {qs: "", face: "https://i.imgur.com/HxFCozp.jpg", faceTo: "https://i.imgur.com/HxFCozp.jpg", faceName: "", confidenceLevel: ""});
+    var face  = await getPhoto(req)
+    //if no photo
+    if(face == "fuck"){
+        console.log("caught")
+        res.render('index', {qs: req.query});
+        return
+    }
+    console.log(face)
+    var faceId = await detect(face);
+    //if not matches or API is down
+    if(typeof faceId == 'undefined'){
+        res.render('result', {qs: "", face: "https://i.imgur.com/hZ3Ngi6.jpg", faceTo: "https://i.imgur.com/hZ3Ngi6.jpg", faceName: "", confidenceLevel: ""});
     }else{
         var faceList = await find(faceId.faceId, 'billion');
         var topResult = faceList[0]['persistedFaceId'];
         var topResultConfidence = Math.ceil(faceList[0]['confidence'] * 100);
-        console.log(topResult);
-        console.log(faceList[0]);
         var topImage = "";
         var topImageName = "";
         //This is used to search through json for url to use
@@ -44,12 +75,10 @@ app.post('/find', urlencodedParser,async function(req, res){
             break;
             }
         }
-        console.log(topImage)
         res.render('result', {qs: req.query, face: face, faceTo: topImage, faceName: topImageName, confidenceLevel: topResultConfidence});
 }});
 
-
-async function detect(photo, faceList){ 
+async function detect(photo){ 
     return new Promise(function(resolve, reject){request({
         headers: {
             'token' : 'AVeJyhfQVU25HEDtzA5GQ7fkjDqs7d',
@@ -58,10 +87,8 @@ async function detect(photo, faceList){
         uri: 'http://157.245.127.122:6978/detect',
         method: 'GET'
       }, function (err, res, body) {
-            console.log(body);
             try{
                 result = JSON.parse(body);
-                console.log(result[0])
                 resolve(result[0]);
             }catch(e){
                 resolve("");
